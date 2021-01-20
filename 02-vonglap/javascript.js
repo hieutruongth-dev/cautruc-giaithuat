68e96f89c48d323e31c94cd578ca1c42ae185ef3
Hieu <hieutruong0711@gmail.com>
2021-01-20 22:20:41 +0700

    bai 2

--- 02-vonglap/javascript.js
+++ 02-vonglap/javascript.js
@@ -1,25 +1,33 @@
+function showMess(a) {
+    var list = [];
+    for (let i = 0; i < 10; i++) {
+        list.push(a);
+    }
+    return list;
+}
+
 function tong1N(n) {
     var sum = 0;
-    for (let i = 1; i<= n; i++) {
-        sum = sum + i ;
+    for (let i = 1; i <= n; i++) {
+        sum = sum + i;
     }
     return ` tổng từ 1 đến ${n} là: ` + sum;
 }
 
-function tongADenB(a,b) {
-    var sum=0;
-    for(let i = a ; i<= b ;i++){
-        sum = sum+ i;
+function tongADenB(a, b) {
+    var sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum = sum + i;
     }
     return `Tổng từ ${a} đến ${b} là: ` + sum;
 }
 
-function tongLe(a,b) {
-    var sum =0;
+function tongLe(a, b) {
+    var sum = 0;
     for (let i = a; i <= b; i++) {
-       if(i%2 != 0){
-           sum += i;
-       } 
+        if (i % 2 != 0) {
+            sum += i;
+        }
     }
     return ` Tổng lẻ từ ${a} đến ${b} là: ` + sum;
 }
@@ -28,7 +36,7 @@ function mangLe(n) {
     var list = [];
     var i = 0;
     while (list.length < n) {
-        if(i %2 != 0){
+        if (i % 2 != 0) {
             list.push(i);
         }
         i++;
@@ -36,13 +44,53 @@ function mangLe(n) {
     return ` mảng ${n} số lẻ nhỏ nhất là: ` + list;
 }
 
-function mangLeTuN(a,b) {
+function mangLeTuN(a, b) {
+    var c = b;
     var list = [];
     while (list.length < a) {
-        if(b%2 !=0){
+        if (b % 2 != 0) {
             list.push(b);
         }
         b++;
     }
-    return  ` mảng ${a} số lẻ bắt đầu từ ${b} là: `+  list;
+    return ` mảng ${a} số lẻ bắt đầu từ ${c} là: ` + list;
+}
+
+function KiemTraNT(n) {
+    var check = true;
+    if (n < 2) {
+        check = false;
+    }
+    else {
+        for (var i = 2; i < n - 1; i++) {
+            if (n % i == 0) {
+                check = false;
+                break;
+            }
+        }
+    }
+    return check;
+
+}
+
+function nNguyenTo(x) {
+    var list = [];
+    var n = 1 ;
+    while (list.length < x) {
+        if(KiemTraNT(n)== true){
+            list.push(n);
+        }n++;    
+    }
+    return list;
+}
+
+function nNguyenToTuM(a,b) {
+    var list = [];
+    var n = b ;
+    while (list.length < a) {
+        if(KiemTraNT(n)== true){
+            list.push(n);
+        }n++;    
+    }
+    return list;
 }
